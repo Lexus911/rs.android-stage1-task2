2a2fe0ae900ed01d69c6732c54cc61e903b999e2
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2020-04-13 23:57:06 +0300

    feat: add solution for subtask2

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,23 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val hours = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve")
+        val minutes = arrayOf("one", "two", "three", "four","five","six","seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+            "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine")
+        val a = hour.toInt()
+        val b = minute.toInt()
+
+        if (b == 0){return "${hours[a]} o' clock"}
+        if (b == 15){return "quarter past ${hours[a]}"}
+        if (b == 30){return "half past ${hours[a]}"}
+        if (b == 40){return "twenty minutes to ${hours[a + 1 ]}"}
+        if (b == 45){return "quarter to ${hours[a + 1]}"}
+        if (b in 1..30){return "${minutes[b -1]} minutes past ${hours[a]}"}
+        if (b in 31..60) {return "${minutes[60 - b - 1]} minutes to ${hours[a + 1]}"}
+        else
+            return ""
+
     }
+
 }
